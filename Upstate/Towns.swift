a8672b55a621c8e6642834c1f6b56320256acc67
Jose Carlos <josealarconchacon@gmail.com>
2022-02-08 14:45:22 -0500

    set Towns model

---
+++ Upstate/Towns.swift
@@ -0,0 +1,16 @@
+//
+//  Towns.swift
+//  Upstate
+//
+//  Created by Jose Alarcon Chacon on 2/8/22.
+//
+
+import Foundation
+
+struct Towns {
+    let city: String
+    let image: String
+    let name: String
+    let state: String
+    let type: String
+}
